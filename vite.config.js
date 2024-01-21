c1f56248f71aaf0dff937fae5e43391195066225
Temesvári Zoltán <117353897+temeszoli@users.noreply.github.com>
2024-01-21 04:49:11 +0100

    Update vite.config.js
    nem tudom mit kéne átírni, hogy működjön a github pages.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  //base: "https://temeszoli.github.io/vue_todo/",
+  base: "https://temeszoli.github.io/vue_todo/",
 });
